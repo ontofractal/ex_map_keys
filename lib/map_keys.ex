49a871d8b0ff070746521baaa24fea8378801ffe
ontofractal <ontofractal@protonmail.com>
2018-09-14 16:56:49 +0100

    new project (ﾉ･o･)ﾉ

---
+++ lib/map_keys.ex
@@ -0,0 +1,18 @@
+defmodule MapKeys do
+  @moduledoc """
+  Documentation for MapKeys.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapKeys.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/map_keys_test.exs
@@ -0,0 +1,8 @@
+defmodule MapKeysTest do
+  use ExUnit.Case
+  doctest MapKeys
+
+  test "greets the world" do
+    assert MapKeys.hello() == :world
+  end
+end
