e3444dbf65ec503e5f265d3dc7f217988fc5adb5
ontofractal <ontofractal@protonmail.com>
2018-09-15 14:01:12 +0100

    first iteration focused on idiomatic and easy to remember fn names

--- lib/map_keys.ex
+++ lib/map_keys.ex
@@ -12,7 +12,19 @@ defmodule MapKeys do
       :world
 
   """
-  def hello do
-    :world
+  def to_atoms_unsafe!(map) do
+    KeyTools.atomize_keys(map)
+  end
+
+  def to_strings(map) do
+    KeyTools.stringify_keys(map)
+  end
+
+  def to_snake_case(map) do
+    ProperCase.to_snake_case(map)
+  end
+
+  def to_camel_case(map) do
+    ProperCase.to_camel_case(map)
   end
 end
