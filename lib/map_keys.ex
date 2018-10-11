8fd6831c62f606dc0f14c6e4601a38f8f8c838fa
ontofractal <ontofractal@protonmail.com>
2018-10-11 11:37:23 +0100

    add docs and specs

--- lib/map_keys.ex
+++ lib/map_keys.ex
@@ -1,29 +1,36 @@
 defmodule MapKeys do
   @moduledoc """
-  Documentation for MapKeys.
+  Documentation for MapKeys
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MapKeys.hello()
-      :world
-
+  Converts string keys to atoms, may cause OTP crash by reaching limit of atoms
   """
+  @spec to_atoms_unsafe!(map) :: map
   def to_atoms_unsafe!(map) do
     KeyTools.atomize_keys(map)
   end
 
+  @doc """
+  Converts atom keys to strings
+  """
+  @spec to_strings(map) :: map
   def to_strings(map) do
     KeyTools.stringify_keys(map)
   end
 
+  @doc """
+  Converts map keys to snake_case
+  """
+  @spec to_snake_case(map) :: map
   def to_snake_case(map) do
     ProperCase.to_snake_case(map)
   end
 
+  @doc """
+  Converts map keys to camelCase
+  """
+  @spec to_camel_case(map) :: map
   def to_camel_case(map) do
     ProperCase.to_camel_case(map)
   end
--- mix.exs
+++ mix.exs
@@ -21,8 +21,6 @@ defmodule MapKeys.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:key_tools, "~> 0.4.0"},
       {:atomic_map, "~> 0.9.2"},
       {:proper_case, "~> 1.0"}
